ca264f12c236d89a8de2bbc453db0f6af70a630e
ginbar <g.inaciobarbosa@gmail.com>
2018-08-17 20:47:10 -0300

    improving lodash extension at db.js

--- api/db.js
+++ api/db.js
@@ -12,9 +12,9 @@ const adapter = new FileAsync(config.dbjson, {
 const db = dblow(adapter);
 
 db.then(db => db._.mixin({
-    select: function (array, ...properties) {
-        return array.map(function(elem) {
-            let obj = {}
+    select(array, ...properties) {
+        return array.map(elem => {
+            let obj = {};
             for (const prop of properties)
                 obj[prop] = elem[prop];
             return obj;
@@ -23,80 +23,3 @@ db.then(db => db._.mixin({
 }));
 
 module.exports = db;
-
-const data = {
-    users: [
-        {
-            name: 'todd',
-            email: 'todd@mail.com',
-        },
-        {
-            name: 'kelly',
-            email: 'kelly@mail.com'
-        },
-        {
-            name: 'trace',
-            email: 'trace@mail.com'
-        },
-        {
-            name: 'bob',
-            email: 'bob@mail.com'
-        }
-    ],
-    posts: [
-        {
-            id: 1,
-            user: 'kelly',
-            title: 'A tl;dr post',
-            text: 'A really long text.'
-        },
-        {
-            id: 2,
-            user: 'trace',
-            title: 'Web services using Node.js',
-            text: 'Some node stuff.',
-            comments: [
-                {
-                    user: 'bob',
-                    date: new Date(),
-                    text: 'Thanks for the node stuff.'
-                }
-            ]
-        },
-        {
-            id: 3,
-            user: 'bob',
-            title: 'Intro to Javascript',
-            text: 'Some js basics.',
-            comments: [
-                {
-                    user: 'trace',
-                    date: new Date(),
-                    text: 'Nice js intro.',
-                    responses: [
-                        {
-                            user: 'bob',
-                            text: 'Thanks.'
-                        }
-                    ]
-                },
-                {
-                    user: 'todd',
-                    date: new Date(Date.now() + 10000000),
-                    text: 'Good job.'
-                },
-                {
-                    user: 'trace',
-                    date: new Date(Date.now() + 20000000),
-                    text: 'Could you make an asm intro?'
-                }
-            ]
-        },
-        {
-            id: 4,
-            user: 'bob',
-            title: 'Wasm for beginners',
-            text: 'Some asm basics.',
-        }
-    ]
-};
